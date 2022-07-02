ddbe91d5f3172383e2056459dd6561b4b73776ff
Philip Simpson <pwsjas433@hotmail.com>
2022-07-02 00:53:01 +0000

    sum all numbers in an array of any number of nested arrays

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function sumItems(array) {
   // Sum all the numbers in the array
-}
-
+  let sum = 0;
+  array.forEach((value) => {
+    if (Array.isArray(value)) {
+      sum += sumItems(value);
+    } else {
+      sum += value;
+    }
+  })
+  return sum;
+};
 module.exports = sumItems;
